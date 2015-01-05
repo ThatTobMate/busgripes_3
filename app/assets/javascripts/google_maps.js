98ca29d4f926f627fd8a5d16d1152e7a843f0e4d
jodykingzett <jody@jodykingzett.com>
2015-01-05 09:52:48 +0000

    multiple changes made styling and page layout, issues with gripe show page, devise views, and js broken with maps

--- app/assets/javascripts/google_maps.js
+++ app/assets/javascripts/google_maps.js
@@ -1,7 +1,3 @@
-//for (i = 0; i < cars.length; i++) { 
-//     text += cars[i] + "<br>";
-// }
-
 var mapStyle = [{"featureType":"water","stylers":[{"visibility":"on"},{"color":"#b5cbe4"}]},{"featureType":"landscape","stylers":[{"color":"#efefef"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#83a5b0"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#bdcdd3"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e3eed3"}]},{"featureType":"administrative","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"road"},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{},{"featureType":"road","stylers":[{"lightness":20}]}]
 
 $(document).ready(function(){
@@ -15,20 +11,23 @@ $(document).ready(function(){
   gripeMarkers = []
   // nb can attach any other info to marker here
   $.each(gripes_data, function(i,gripe){
-    
     var gripe_marker = {"lat": gripe.latitude, "lng": gripe.longitude}  
     gripeMarkers.push(gripe_marker)
+  });
 
   })
+
     markers = handler.addMarkers(gripeMarkers);
     handler.bounds.extendWith(markers);
     handler.fitMapToBounds();
-  });
-});
 
-    // provider: {
-    //   zoom:      15,
-    //   center:    new google.maps.LatLng(53.385873, -1.471471),
-    //   mapTypeId: google.maps.MapTypeId.ROADMAP,
-    //   styles:    mapStyle
+    // heatmap = new google.maps.visualization.HeatmapLayer({
+    // data: gripeMarkers});
+    // heatmap.setMap(map);
+    // function toggleHeatmap(){
+    // heatmap.setMap(heatmap.getMap() ? null : map);
     // }
+
+  });
+// });
+
