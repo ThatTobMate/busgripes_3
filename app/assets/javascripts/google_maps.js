2616d53c3373f3504253c4c8ed1c206ffcfde1c1
jodykingzett <jody@jodykingzett.com>
2015-01-06 14:18:50 +0000

    heatmaps made - completely changed the gmaps4rails script

--- app/assets/javascripts/google_maps.js
+++ app/assets/javascripts/google_maps.js
@@ -1,33 +1,90 @@
 var mapStyle = [{"featureType":"water","stylers":[{"visibility":"on"},{"color":"#b5cbe4"}]},{"featureType":"landscape","stylers":[{"color":"#efefef"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#83a5b0"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#bdcdd3"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e3eed3"}]},{"featureType":"administrative","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"road"},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{},{"featureType":"road","stylers":[{"lightness":20}]}]
 
 $(document).ready(function(){
-  handler = Gmaps.build('Google');
-  handler.buildMap({ provider: {
+  // heatmap = null;
+  // handler = Gmaps.build('Google');
+  // handler.buildMap({ 
+  //   provider: {
+  //     zoom: 15,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP,
+  //     styles: mapStyle
+  //   }, 
+  //   internal: {
+  //     id: 'map'
+  //   }
+  // }, function() {
+  //   gripeMarkers = []
+  //   // nb can attach any other info to marker here
+  //   $.each(gripes_data, function(i,gripe){
+  //     var gripe_marker = {"lat": gripe.latitude, "lng": gripe.longitude}  
+  //     gripeMarkers.push(gripe_marker)
+  //   });
+
+  //   var m = handler.getMap();
+
+  //   markers = handler.addMarkers(gripeMarkers);
+  //   handler.bounds.extendWith(markers);
+  //   handler.fitMapToBounds();
+
+  //   var pointArray = new google.maps.MVCArray(gripeMarkers);
+
+  //   heatmap = new google.maps.visualization.HeatmapLayer({data: pointArray});
+
+  //   heatmap.setMap(m);
+
+  //   console.log(m)
+
+
+
+function initialize() {
+  var mapOptions = {
     zoom: 15,
+    center: new google.maps.LatLng(51.5072, 0.1275),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: mapStyle
+  };
 
-  }, internal: {id: 'map'}}, function(){
-  gripeMarkers = []
-  // nb can attach any other info to marker here
-  $.each(gripes_data, function(i,gripe){
-    var gripe_marker = {"lat": gripe.latitude, "lng": gripe.longitude}  
-    gripeMarkers.push(gripe_marker)
+  map = new google.maps.Map(document.getElementById('map'),
+      mapOptions);
+
+  var gripeMarkers = [];
+  var bounds = new google.maps.LatLngBounds();
+
+    // nb can attach any other info to marker here
+  $.each(gripes_data, function(i, gripe){
+    var gripe_marker = new google.maps.LatLng(gripe.latitude, gripe.longitude);
+    bounds.extend(gripe_marker);
+    gripeMarkers.push(gripe_marker);
   });
 
-  })
+  // bounds.extendWith(gripeMarkers);
+  // map.fitMapToBounds();
 
-    markers = handler.addMarkers(gripeMarkers);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
+  map.fitBounds(bounds)
 
-    // heatmap = new google.maps.visualization.HeatmapLayer({
-    // data: gripeMarkers});
-    // heatmap.setMap(map);
-    // function toggleHeatmap(){
-    // heatmap.setMap(heatmap.getMap() ? null : map);
-    // }
 
+  console.log(gripeMarkers)
+
+
+  var pointArray = new google.maps.MVCArray(gripeMarkers);
+
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: pointArray
   });
-// });
+
+  heatmap.setMap(map);
+}
+
+// function toggleHeatmap() {
+//   heatmap.setMap(heatmap.getMap() ? null : map);
+// }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+});
+
+  // $("#toggleHeatmap").on("click", function(){
+  //   debugger
+  //   // heatmap.setMap(heatmap.getMap() ? null : map);
+  // })
+    
 
